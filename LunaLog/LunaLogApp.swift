f287ef82edb2ef5d0fd348d7a4a539a94a6debf3
Fajar Rashid <fajarrashid786@gmail.com>
2024-07-15 14:46:09 -0400

    Initial Commit

---
+++ LunaLog/LunaLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunaLogApp.swift
+//  LunaLog
+//
+//  Created by Fajar Rashid on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct LunaLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
