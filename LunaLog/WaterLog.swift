4a0447496e5169994199250e5ae58131397d56a0
Fajar Rashid <fajarrashid786@gmail.com>
2024-07-17 22:03:25 -0400

    Water Tracker (Need to change colors/fonts) - done

--- LunaLog/WaterLog.swift
+++ LunaLog/WaterLog.swift
@@ -5,13 +5,116 @@
 //  Created by Fajar Rashid on 7/17/24.
 //
 
+// Need to change color and background
 import SwiftUI
+import UserNotifications
 
 struct WaterLog: View {
+    @State private var totalWater: Double = 0
+    private let dailyGoal: Double = 3000 // Daily goal in ml
+    
     var body: some View {
-        Text("Water Log Page")
-            .font(.largeTitle)
+        VStack {
+            Text("Water Tracker")
+                .font(.largeTitle)
+                .padding()
+            
+            Text("\(Int(totalWater)) ml / \(Int(dailyGoal)) ml")
+                .font(.title)
+                .padding()
+            
+            ProgressView(value: totalWater, total: dailyGoal)
+                .progressViewStyle(LinearProgressViewStyle())
+                .padding()
+            
+            Text("Consumed: \(Int(totalWater / dailyGoal * 100))%")
+                .font(.title2)
+                .padding()
+            
+            HStack {
+                Button(action: {
+                    addWater(amount: 100)
+                }) {
+                    Text("Add 100ml")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                
+                Button(action: {
+                    addWater(amount: 200)
+                }) {
+                    Text("Add 200ml")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                
+                Button(action: {
+                    addWater(amount: 500)
+                }) {
+                    Text("Add 500ml")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+            }
             .padding()
+            
+            Button(action: {
+                resetWater()
+            }) {
+                Text("Reset")
+                    .padding()
+                    .background(Color.red)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            .padding()
+            
+            Spacer()
+        }
+        .onAppear(perform: setupNotifications)
+    }
+    
+    private func addWater(amount: Double) {
+        totalWater += amount
+        if totalWater > dailyGoal {
+            totalWater = dailyGoal
+        }
+    }
+    
+    private func resetWater() {
+        totalWater = 0
+    }
+    
+    private func setupNotifications() {
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+            if granted {
+                scheduleNotification()
+            }
+        }
+    }
+    
+    private func scheduleNotification() {
+        let content = UNMutableNotificationContent()
+        content.title = "Stay Hydrated!"
+        content.body = "Don't forget to drink water today."
+        content.sound = UNNotificationSound.default
+        
+        var dateComponents = DateComponents()
+        dateComponents.hour = 10 // Reminder at 10 AM
+        dateComponents.minute = 0
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request)
     }
 }
 
@@ -20,3 +123,5 @@ struct WaterLog_Previews: PreviewProvider {
         WaterLog()
     }
 }
+
+
