1dee1f33264a775ae373c2bbbecdb169a4a525c6
nira <nira93819@gmail.com>
2024-07-18 13:42:26 -0400

    reward

--- LunaLog/Rewards.swift
+++ LunaLog/Rewards.swift
@@ -4,20 +4,37 @@
 //
 //  Created by Fajar Rashid on 7/17/24.
 //
-
 import SwiftUI
 
 struct Rewards: View {
+    @EnvironmentObject var pointsManager: PointsManager // Access the shared PointsManager
+
     var body: some View {
-        Text("Rewards Page")
-            .font(.largeTitle)
-            .padding()
+        VStack {
+            Text("Rewards Page")
+                .font(.largeTitle)
+                .padding()
+            
+            Text("Points: \(pointsManager.points)")
+                .font(.title)
+                .padding()
+                .foregroundColor(.white)
+
+            if pointsManager.points >= 50 {
+                Text("Congratulations! You earned a reward!")
+                    .font(.title)
+                    .foregroundColor(.green)
+                    .padding()
+            }
+        }
+        .padding()
+        .background(Color.black.edgesIgnoringSafeArea(.all))
     }
 }
 
 struct Rewards_Previews: PreviewProvider {
     static var previews: some View {
-        Rewards()
+        Rewards().environmentObject(PointsManager()) // Provide PointsManager
     }
 }
 
